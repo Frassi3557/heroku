bf162b39603b12c082c734be8b30b3752d6cd2a5
Frassi3557 <freysi_freys@live.com>
2019-02-04 17:08:30 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
@@ -6,15 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 
-    response = requests.get("https://apis.is/petrol.json")
-    print(response.content)
+    resp = requests.get('https://apis.is/petrol')
+    print(resp.content)
+    
 
-
-
-#@app.errorhandler(404)
-#def page_not_found(error):
-    #return render_template('page_not_found.tpl'), 404
-
-#if __name__ == '__main__':
-    #app.run()
+if __name__ == '__main__':
+    app.run()
     #app.run(debug=True, use_reloader=True)
