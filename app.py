fac6450fc31133ee8fc9793a2f5f3b169c358ee1
Frassi3557 <freysi_freys@live.com>
2019-03-19 00:29:50 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -17,18 +17,20 @@ def student():
 #Verkefni 6
 @app.route('/6')
 def index():
-    if 'username' in session:
+    if 'username' == 'admin' in session:
         return 'Logged in as %s' % escape(session['username'])
     return 'You are not logged in'
 
 @app.route('/6/login' , methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+        #if 'username' == 'admin':
+            #session['username'] = request.form['username']
+            #return redirect(url_for('index'))
     return '''
         <form method="post">
             <p><input type=text name=username>
+            
             <p><input type=submit value=Login>
         </form>
     '''
